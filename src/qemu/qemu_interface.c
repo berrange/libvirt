0b8cc6398e35ffe8c556a9dc2a65d13f6231eaf4
Daniel P. Berrangé <berrange@redhat.com>
2018-12-14 12:07:08 +0000

    qemu: use line breaks in command line args written to log
    The QEMU command line arguments are very long and currently all written
    on a single line to /var/log/libvirt/qemu/$GUEST.log. This introduces
    logic to add line breaks after every env variable and "-" optional
    argument, and every positional argument. This will create a clearer log
    file, which will in turn present better in bug reports when people cut +
    paste from the log into a bug comment.
    
    An example log file entry now looks like this:
    
      2018-12-14 12:57:03.677+0000: starting up libvirt version: 5.0.0, qemu version: 3.0.0qemu-3.0.0-1.fc29, kernel: 4.19.5-300.fc29.x86_64, hostname: localhost.localdomain
      LC_ALL=C \
      PATH=/usr/local/bin:/usr/local/sbin:/usr/bin:/usr/sbin \
      HOME=/home/berrange \
      USER=berrange \
      LOGNAME=berrange \
      QEMU_AUDIO_DRV=none \
      /usr/bin/qemu-system-ppc64 \
      -name guest=guest,debug-threads=on \
      -S \
      -object secret,id=masterKey0,format=raw,file=/home/berrange/.config/libvirt/qemu/lib/domain-33-guest/master-key.aes \
      -machine pseries-2.10,accel=tcg,usb=off,dump-guest-core=off \
      -m 1024 \
      -realtime mlock=off \
      -smp 1,sockets=1,cores=1,threads=1 \
      -uuid c8a74977-ab18-41d0-ae3b-4041c7fffbcd \
      -display none \
      -no-user-config \
      -nodefaults \
      -chardev socket,id=charmonitor,fd=23,server,nowait \
      -mon chardev=charmonitor,id=monitor,mode=control \
      -rtc base=utc \
      -no-shutdown \
      -boot strict=on \
      -device qemu-xhci,id=usb,bus=pci.0,addr=0x1 \
      -device virtio-balloon-pci,id=balloon0,bus=pci.0,addr=0x2 \
      -sandbox on,obsolete=deny,elevateprivileges=deny,spawn=deny,resourcecontrol=deny \
      -msg timestamp=on
      2018-12-14 12:57:03.730+0000: shutting down, reason=failed
    
    Signed-off-by: Daniel P. Berrangé <berrange@redhat.com>

--- src/qemu/qemu_interface.c
+++ src/qemu/qemu_interface.c
@@ -368,7 +368,7 @@ qemuCreateInBridgePortWithHelper(virQEMUDriverConfigPtr cfg,
         char ebuf[1024];
         char *errstr = NULL;
 
-        if (!(cmdstr = virCommandToString(cmd)))
+        if (!(cmdstr = virCommandToString(cmd, false)))
             goto cleanup;
         virCommandAbort(cmd);
 
